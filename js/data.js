8b9e7c4fbd7db55871cf5631fca2d8a9ee3f6f99
AceRy07 <ardaakar177@gmail.com>
2026-09-23 11:36:12 +0300

    fix: Update EP release date and remove unused track entries

--- js/data.js
+++ js/data.js
@@ -8,20 +8,4 @@ const TRACKS = [
     src: "assets/audio/4AM (INSTURMENTAL - SMALL VOCALS).mp3",
     cover: "assets/images/4AM.png"
   },
-  {
-    title: "Dime of Hope",
-    subtitle: "Dime of Hope (Demo)",
-    explicit: true,
-    duration: "3:28",
-    src: "assets/audio/4AM (INSTURMENTAL - SMALL VOCALS).mp3",
-    cover: "assets/images/4AM.png"
-  },
-  {
-    title: "4AM",
-    subtitle: "4AM (Demo)",
-    explicit: false,
-    duration: "1:01",
-    src: "assets/audio/4AM (INSTURMENTAL - SMALL VOCALS).mp3",
-    cover: "assets/images/4AM.png"
-  }
 ];
